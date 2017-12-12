1cfc0d21fe8105c1779bbc94472d2713ea764152
Tisa Segovic <te557@nyu.edu>
2017-12-12 15:52:38 +0800

    Update matrix-list.py

--- matrix-list.py
+++ matrix-list.py
@@ -1,2 +1,18 @@
 
-# To do by Brandon
+""" To do by Brandon - let me know when you have finished!
+Fill out the following functions & Feel free to rename them! """
+
+import random
+import numpy as np
+
+def alnum2matrix(msg):
+  
+def matrix_list2matrix_string(matrix):
+  
+def matrix_string2matrix_list(mat_string):
+  
+def randomize_one_digit(matrix):
+  
+def error_correction(matrix):
+  
+def matrix2alnum(matrix):
