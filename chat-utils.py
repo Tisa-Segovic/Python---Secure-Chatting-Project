f541e726da6d9261fcc1a12171cde230e5e98722
Tisa Segovic <te557@nyu.edu>
2017-12-12 16:00:06 +0800

    Update chat-utils.py

--- chat-utils.py
+++ chat-utils.py
@@ -1,2 +1,14 @@
 """ To do By Brandon - Let me know when you are finished!
-"""
+HINT: You should do the Matrix File first, because you need that file to do this file"""
+
+import socket
+import time
+import Matrix
+
+def print_state(state):
+  
+def mysend(s, msg):
+  
+def myrecv(s):
+  
+def text_proc(text, user):
